ed1dbbb25410c27e13eb89cb2019c47a63670a52
yamada.masahiro <yamada.masahiro@gmail.com>
2025-11-08 09:44:28 +0900

    now debug

--- page-etc-result.php
+++ page-etc-result.php
@@ -198,7 +198,11 @@ if(!empty($img)){
   </div>
 </div>
     <div class="product_see_more">
-      <a href="<?php echo get_permalink($post->ID );?>" title="<?php echo get_the_title($post->ID);?>">詳細を見る <i class="fas fa-angle-double-right"></i>
+      <!-- a href="<?php echo get_permalink($post->ID );?>" title="<?php echo get_the_title($post->ID);?>">詳細を見る <i class="fas fa-angle-double-right"></i -->
+			<?php 
+				$url_archives = get_permalink(); 
+      	echo '<a href="' . esc_url($url_archives) . '" title="<?php echo get_the_title($post->ID);?>">詳細を見る <i class="fas fa-angle-double-right"></i>';
+			?>
       </a>
     </div>
 
